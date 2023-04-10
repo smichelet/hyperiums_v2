d176fd5ebda6bd478c2782896227a17110d6b5f3
WooT <lindorya@hotmail.fr>
2023-04-10 23:01:14 +0200

    Configure S3 in website hosting mode

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,6 @@
 module "s3_hyperiums" {
   source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "3.7.0"
+  version = "3.8.2"
 
   bucket = var.s3_bucket_name_hyperiums
   acl    = "private"
@@ -20,6 +20,10 @@ module "s3_hyperiums" {
 
   versioning = {
     enabled = false
+  }
+
+  website = {
+    index_document = "index.html"
   }
 
   tags = {
