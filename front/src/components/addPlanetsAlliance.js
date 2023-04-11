310942d212ea6c56b7d5e77d37a80fa5e4580c0c
WooT <lindorya@hotmail.fr>
2023-04-11 23:31:50 +0200

    Fix bug in addPlanetsAlliances feature

--- front/src/components/addPlanetsAlliance.js
+++ front/src/components/addPlanetsAlliance.js
@@ -8,7 +8,10 @@ export function AddPlanetsAlliance(filePlanetsAlliance, csvFilePlanets) {
     const modifiedLine = modifiedLineArray.join(" ");
     return modifiedLine;
   });
-  const newPlanetsAlliance = newLines.join(" ");
+
+  // Création d'une variable newPlanetsAlliance qui est le tableau des planètes de l'alliance
+  let newPlanetsAlliance = newLines.join(" ");
+  newPlanetsAlliance = newPlanetsAlliance.split(" ")
 
   // Traitement du fichier CSV de la liste complète des planètes afin d'ajouter le tag de notre alliance
   let planetsArray = csvFilePlanets.split("\n");
@@ -17,17 +20,17 @@ export function AddPlanetsAlliance(filePlanetsAlliance, csvFilePlanets) {
   for (let i = 1; i < planetsArray.length; i++) {
     let planet = planetsArray[i].split(",");
     let planetName = planet[1];
-  
+
     // Vérifie si le nom de la planète est dans la liste
-    if (newPlanetsAlliance.indexOf(planetName) !== -1) {
+    if (newPlanetsAlliance.includes(planetName)) {
       // Remplace la valeur du tag "[]" par "[8]"
       planet[8] = "[8]";
     }
-  
+
     // Rejoindre les champs de la planète en une chaîne de caractères et remplacer la ligne dans le tableau
     planetsArray[i] = planet.join(",");
   }
-  
+
   // Rejoindre les lignes du tableau en une chaîne de caractères
   let updatedPlanetsData = planetsArray.join("\n");
 
